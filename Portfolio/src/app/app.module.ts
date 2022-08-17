8ca8fb70f000e94737e09e6537bce79e4ea1da3d
Sebamazu <mazunikseba@gmail.com>
2022-08-17 00:19:33 -0300

    armado de acerca de y banner

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { LogoAPComponent } from './componentes/logo-ap/logo-ap.component';
 import { SocialComponent } from './componentes/social/social.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    SocialComponent
+    SocialComponent,
+    BannerComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
