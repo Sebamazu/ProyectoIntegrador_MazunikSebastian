8e3320848c747919639ba71f0cb6500a69ceb5ab
Sebamazu <mazunikseba@gmail.com>
2022-08-23 18:23:49 -0300

    Trabajo en BackEnd

---
+++ Portfolio/BackEnd/src/main/java/com/portfolio/sm/Security/Controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.portfolio.sm.Security.Controller;
+
+import com.portfolio.sm.Security.Service.RolService;
+import com.portfolio.sm.Security.Service.UsuarioService;
+import com.portfolio.sm.Security.jwt.JwtProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin
+public class AuthController {
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    UsuarioService usuarioService;
+    @Autowired
+    RolService rolService;
+    @Autowired
+    JwtProvider jwtProvider;
+    
+    
+    
+}
